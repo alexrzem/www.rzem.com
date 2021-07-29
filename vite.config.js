420c1128e4fb1164b64408dfa178ea1b5d005b03
Alex <alex@rzem.com>
2021-07-29 18:28:34 +1000

    dist to docs

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import path from 'path'
+import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	build: {
+		outDir: 'docs',
+	},
 	plugins: [vue()],
 	resolve: {
 		alias: {
